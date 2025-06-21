71ddf8ee3ac19bb41cc10d439fb60ae0fe8adefd
Sharad Sharma <rahulsharma199927@gmail.com>
2025-06-21 14:46:01 +0530

    added user form with crud

--- backend/database.py
+++ backend/database.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 #local 
-URL_DATABASE = 'mysql+pymysql://root:root@localhost:3306/myapp'
+# URL_DATABASE = 'mysql+pymysql://root:root@localhost:3306/myapp'
 
 #live
-# URL_DATABASE = 'mysql+pymysql://sql12785307:AxFIQBvdf6@sql12.freesqldatabase.com:3306/sql12785307'
+URL_DATABASE = 'mysql+pymysql://sql12785307:AxFIQBvdf6@sql12.freesqldatabase.com:3306/sql12785307'
 
 engine = create_engine(URL_DATABASE)
 
